0567822a81f5e8c93cbc65366f14289a6687f849
michelelaporta <michele.laporta@gmail.com>
2020-07-03 16:19:10 +0200

    3 files modified, 1 added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,39 @@ import { first } from 'rxjs/operators';
 
 import { User } from '../_models';
 import { UserService } from '../_services';
+import { VgAPI } from 'videogular2/compiled/core';
 
-@Component({ templateUrl: 'home.component.html' })
+export interface IMedia {
+    title: string;
+    src: string;
+    type: string;
+}
+
+@Component({ templateUrl: 'home.component.html',styleUrls: [ './home.component.css' ] })
 export class HomeComponent {
+  playlist: Array<IMedia> = [
+        {
+            title: 'Pale Blue Dot',
+            src: 'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4',
+            type: 'video/mp4'
+        },
+        {
+            title: 'Big Buck Bunny',
+            src: 'http://static.videogular.com/assets/videos/big_buck_bunny_720p_h264.mov',
+            type: 'video/mp4'
+        },
+        {
+            title: 'Elephants Dream',
+            src: 'http://static.videogular.com/assets/videos/elephants-dream.mp4',
+            type: 'video/mp4'
+        }
+    ];
     loading = false;
     users: User[];
+    api: VgAPI;
+    currentIndex = 0;
+    //currentItem: IMedia = this.playlist[ this.currentIndex ];
+    currentItem: IMedia = this.playlist[ this.currentIndex ];
 
     constructor(private userService: UserService) { }
 
@@ -17,5 +45,13 @@ export class HomeComponent {
             this.loading = false;
             this.users = users;
         });
+    }
+
+    onPlayerReady(api: VgAPI) {
+       this.api = api;
+   }
+
+   playVideo() {
+        this.api.play();
     }
 }
